8136456b7e6b8ff073ba74787a9a21e4e717bcd1
raffdoc <raffdoc@yahoo.com>
2014-05-13 07:58:52 +0200

    New GFR function added (simplified form).

--- R/glomerular_filatration_rage.R
+++ R/glomerular_filatration_rage.R
@@ -1,4 +1,5 @@
-gfr <- function(gender="female|male",scr=NULL,scr.u="mg/dl"|"µmol/l",age=NULL, race="black|white"){
+## Glomerular filatration rate by CKD-EPI
+gfr_ckd <- function(gender="female|male",scr=NULL,scr.u="mg/dl"|"µmol/l",age=NULL, race="black|white"){
   ## from A New Equation to Estimate Glomerular Filtration Rate
   ## Model should be reweritten as in article - Sun May  4 21:33:42 CEST 2014
   ## Creatinine levesl must be converted from mg/dL to µmol/l and viceversa with facor of mg/dl  88.4	µmol/l
@@ -20,4 +21,13 @@ gfr <- function(gender="female|male",scr=NULL,scr.u="mg/dl"|"µmol/l",age=NULL, 
                 } else  gfr <- 141*((scr/0.9)^-1.209)*((0.993)^age); return(gfr)
         } else  
   return(gfr)
+}
+
+
+## glomerular filtration rate by Cockcroft -Gault
+gfr_cg <- function(gender="female|male",age=NULL,weight=NULL,scr=NULL,scr.u="mg/dl"|"µmol/l"){
+        if (scr.u=="mg/dl"){scr <- scr*88.4} 
+        if (gender=="female"){
+                gfr <- (1.04*weight*(140-age))/scr; return(gfr)
+        } else  gfr <- (1.23*weight*(140-age))/scr; return(gfr)
 }
